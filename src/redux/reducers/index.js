8589c945597b1bc0193cac33e3eb4f5e7e13ce1e
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-07-12 18:51:27 +0300

    Delete src directory

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import { productReducer, selectedProductsReducer } from "./productReducer";
-
-const reducers=combineReducers({
-    allProducts:productReducer,
-    product:selectedProductsReducer
-});
-
-export default reducers
